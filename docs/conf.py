4880a7cbed435b6c9650950c759e22afc84ec219
mmRoshani <mohammadmojtabaroshani@outlook.com>
2024-12-21 01:33:07 +0330

    OPS[app]: github action

--- docs/conf.py
+++ docs/conf.py
@@ -21,14 +21,12 @@ author = "MohammadMojtaba Roshani"
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    "sphinx.ext.duration",
-    "sphinx.ext.doctest",
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
 ]
-templates_path = ["_templates"]
+templates_path = ["templates"]
 exclude_patterns = [
-    "_build",
+    "build",
     "Thumbs.db",
 ]
 
@@ -37,4 +35,4 @@ exclude_patterns = [
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["static"]
+htmlstatic_path = ["static"]
